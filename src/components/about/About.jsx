fbece6a670fc20d43004a6d04e07e4f5c0987bf2
IbrahimMalgwi <ibrahimganamalgwi@outlook.com>
2023-08-23 11:00:20 +0100

    Completed about me component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,12 +1,50 @@
 import React from 'react'
 import "./about.css"
+import ME from "../../assets/me-about.jpg"
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
   return (
-    <section id='about'>
-      About
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="AboutImage" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>300+ World Wide </small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>20+ Completed</small>
+            </article>
+          </div>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem ullam temporibus voluptates dolore vel, rerum iusto unde nisi mollitia eaque provident, blanditiis ea excepturi quo minima quis cumque modi saepe.</p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
 export default About
