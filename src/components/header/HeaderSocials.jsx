c6043e5f8ecccc4bc81bd4e18d384b4acf5be917
IbrahimMalgwi <ibrahimganamalgwi@outlook.com>
2023-08-25 00:19:57 +0100

    Refactored headerSocial

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,50 +7,30 @@ import { BsInstagram } from "react-icons/bs"
 const HeaderSocial = () => {
   return (
     <div className="header__socials">
-      <a
-        href="https://www.linkedin.com/in/ibrahim-gana-malgwi/"
+      <a href="https://www.linkedin.com/in/ibrahim-gana-malgwi/"
         target="_blank"
         rel="noreferrer"
-      >
-        {" "}
-        <BsLinkedin />{" "}
-      </a>
+      ><BsLinkedin /> </a>
       
-      <a
-        href="https://github.com/IbrahimMalgwi"
+      <a href="https://github.com/IbrahimMalgwi"
         target="_blank"
         rel="noreferrer"
-      >
-        {" "}
-        <FaGithub />{" "}
-      </a>
+      > <FaGithub /> </a>
 
-      <a
-        href="https://twitter.com/GanzyMalgwi"
+      <a href="https://twitter.com/GanzyMalgwi"
         target="_blank"
         rel="noreferrer"
-      >
-        {" "}
-        <BsTwitter />{" "}
-      </a>
+      > <BsTwitter /> </a>
 
-      <a
-        href="https://www.facebook.com/GanzyMalgwi"
+      <a href="https://www.facebook.com/GanzyMalgwi"
         target="_blank"
         rel="noreferrer"
-      >
-        {" "}
-        <BsFacebook />{" "}
-      </a>
+      > <BsFacebook /> </a>
 
-      <a
-        href="https://www.instagram.com/ganzymalgwi/"
+      <a href="https://www.instagram.com/ganzymalgwi/"
         target="_blank"
         rel="noreferrer"
-      >
-        {" "}
-        <BsInstagram />{" "}
-      </a>
+      > <BsInstagram /> </a>
     </div>
   );
 }
